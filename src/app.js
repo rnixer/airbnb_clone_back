e89697a22a93b2b2e933db00ce2efab18fe784e8
rnixer <kewell700@hotmail.com>
2024-02-12 00:30:36 +0700

    set_up

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", (req, res, next) => {
+  res.json("test");
+});
+
+app.post("/register", (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body;
+  res.json({ name, email, password, confirmPassword });
+});
+
+app.listen(8777, () => console.log("service is running in port 8777"));
