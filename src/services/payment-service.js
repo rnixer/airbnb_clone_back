22280034cbd3a9acbbbb6249caf16633da76fb7c
rnixer <kewell700@hotmail.com>
2024-02-21 16:08:40 +0700

    fix bug filter and payment

--- src/services/payment-service.js
+++ src/services/payment-service.js
@@ -8,5 +8,5 @@ exports.createPayment = (data) =>
 
 exports.getPaymentByPayerId = (
   payerId
-) => prisma.$queryRaw` select pro.id, pro.property_name , pro.user_id as createId ,pa.id,pa.status, b.id,b.total_price,u.name,u.id as payerId ,b.image as slipImage from  propertys pro join payment pa on pa.property_id = pro.id join users u on pa.user_id = u.id join bookings b on pa.booking_id = b.id   where (select pro.user_id from propertys pro join payment pa on pa.property_id = pro.id 
-  where ${payerId} = pa.user_id  ) `;
+) => prisma.$queryRaw` select pro.id, pro.property_name , pro.user_id as createId ,pa.id,pa.status, b.id,b.total_price,u.name,u.id as payerId ,b.image as slipImage from  propertys pro join payment pa on pa.property_id = pro.id join users u on pa.user_id = u.id join bookings b on pa.booking_id = b.id    
+where ${payerId} = pro.user_id  `;
--- src/services/place-service.js
+++ src/services/place-service.js
@@ -33,12 +33,14 @@ exports.getAllMyPlace = (userId) =>
 exports.getAllPlace = () => prisma.property.findMany();
 
 exports.filter = (checkInDate, checkOutDate, num_guests) =>
-  prisma.$queryRaw`select distinct(p.property_name)
-, p.id , p.address,p.description,p.image,p.nightly_price
- from bookings b right join propertys p on b.property_id = p.id
-
-   WHERE (  b.checkin_date IS NULL and checkout_date is null )
-      or ( b.checkin_date  and b.checkout_date not between ${checkInDate} and ${checkOutDate})
-         and ( ${checkInDate} and ${checkOutDate} not between b.checkin_date  and b.checkout_date  )
-         and ${num_guests} <= p.num_guests
-         `;
+  prisma.$queryRaw` select p.id,p.property_name , p.num_guests
+  , b.checkin_date,b.checkout_date 
+  from bookings b right join  propertys p on b.property_id = p.id  where p.id not in (select p.id
+ --   , p.id,b.property_id , p.address,b.checkin_date,b.checkout_date,b.total_price,p.num_guests -- บรรทัดนี้เอาออกซะ
+  from bookings b right join  propertys p on b.property_id = p.id
+ where 
+ ${checkInDate} between b.checkin_date and b.checkout_date 
+ or (${checkInDate}  < b.checkin_date and ${checkOutDate} >  b.checkout_date )
+ or ( ${checkOutDate} between b.checkin_date and  b.checkout_date )
+ or ( ${checkInDate} and ${checkOutDate} between b.checkin_date and  b.checkout_date))
+ and ${num_guests} <= p.num_guests  `;
